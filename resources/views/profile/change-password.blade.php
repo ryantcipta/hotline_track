040edeab6aa516930398bc8b79a87a8ab33132bd
ryantcipta <ryantika3@gmail.com>
2025-01-07 14:37:47 +0800

    perbaikan css

--- resources/views/profile/change-password.blade.php
+++ resources/views/profile/change-password.blade.php
@@ -7,10 +7,10 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Change Password</title>
-        
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="{{url("css/styles.css")}}" rel="stylesheet" />
         <link href="{{url("css/new.css")}}" rel="stylesheet" />
+        <link rel="stylesheet" href="{{url("css/style_card.css")}}">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
         
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,13 +7,11 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Profile</title>
-        
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="{{url("css/styles.css")}}" rel="stylesheet" />
         <link href="{{url("css/new.css")}}" rel="stylesheet" />
+        <link rel="stylesheet" href="{{url("css/style_card.css")}}">
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-
-       
     </head>
     <body class="sb-nav-fixed">
      
