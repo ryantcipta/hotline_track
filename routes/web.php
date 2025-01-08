7b4c4c6f7efd7550665c3a7832d0b9d423296fb3
ryantcipta <ryantika3@gmail.com>
2025-01-08 08:51:23 +0800

    hapus file yang tidak perlu

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::post('/login', [UserController::class,'login'])->name('auth.login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
  
 //route tracking
-Route::get('/tracking-hotline', [TrackingController::class, 'index']);
+Route::get('/', [TrackingController::class, 'index']);
 Route::get('/search', [TrackingController::class, 'index'])->name('search');
 
 
@@ -63,53 +63,53 @@ Route::middleware('auth')->group(function () {
 });
 
 
-Route::get('/forgot-password', function(){
-    return view('auth.forgot-password');
-})->middleware('guest')->name('password.request');
+// Route::get('/forgot-password', function(){
+//     return view('auth.forgot-password');
+// })->middleware('guest')->name('password.request');
 
-Route::post('/forgot-password', function  (Request $request)
-{
-    $request->validate(['email' => 'required|email']);
+// Route::post('/forgot-password', function  (Request $request)
+// {
+//     $request->validate(['email' => 'required|email']);
 
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
+//     $status = Password::sendResetLink(
+//         $request->only('email')
+//     );
 
-    return $status === Password::RESET_LINK_SENT
-    ? back()->with(['status' => __($status)])
-    : back()->withErrors(['email' => __($status)]);
+//     return $status === Password::RESET_LINK_SENT
+//     ? back()->with(['status' => __($status)])
+//     : back()->withErrors(['email' => __($status)]);
 
-})->middleware('guest')->name('password.email');
+// })->middleware('guest')->name('password.email');
 
-Route::get('/reset-password/{token}', function (string $token){
-    return view('auth.reset-password', ['token' => $token]);
+// Route::get('/reset-password/{token}', function (string $token){
+//     return view('auth.reset-password', ['token' => $token]);
 
-})->middleware('guest')->name('password.reset');
+// })->middleware('guest')->name('password.reset');
 
-Route::post('/reset-password', function (Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:8|confirmed',
-    ]);
+// Route::post('/reset-password', function (Request $request) {
+//     $request->validate([
+//         'token' => 'required',
+//         'email' => 'required|email',
+//         'password' => 'required|min:8|confirmed',
+//     ]);
  
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function (User $user, string $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
+//     $status = Password::reset(
+//         $request->only('email', 'password', 'password_confirmation', 'token'),
+//         function (User $user, string $password) {
+//             $user->forceFill([
+//                 'password' => Hash::make($password)
+//             ])->setRememberToken(Str::random(60));
  
-            $user->save();
+//             $user->save();
  
-            event(new PasswordReset($user));
-        }
-    );
+//             event(new PasswordReset($user));
+//         }
+//     );
  
-    return $status === Password::PASSWORD_RESET
-                ? redirect()->route('auth.login')->with('status', __($status))
-                : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password.update');
+//     return $status === Password::PASSWORD_RESET
+//                 ? redirect()->route('auth.login')->with('status', __($status))
+//                 : back()->withErrors(['email' => [__($status)]]);
+// })->middleware('guest')->name('password.update');
 
 
 Route::group(['middleware'=>['auth']], function(){
